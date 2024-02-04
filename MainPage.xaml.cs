0e13da1706fdbe6352780f3c5e014aedb7b330de
V <vitoralcanfor@hotmail.com>
2024-02-04 17:06:46 -0300

    chore: removed event handler

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -13,18 +13,6 @@ namespace Detail_Shopping
             
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
-
         private void BtnScan_OnClicked(object? sender, EventArgs e)
         {
             Shell.Current.GoToAsync(nameof(ScanPage));
